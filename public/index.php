e73ddb41b3b9df10d5e78907e3671b3ac7405e98
yo <causawey69420@gmail.com>
2026-04-23 12:14:15 +0200

    aaa

--- public/index.php
+++ public/index.php
@@ -1,20 +1,11 @@
-<?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Danmakudle</title>
+</head>
+<body>
+    <h1>DANMAKUDLE</h1>
+</body>
+</html>
