838d229432043edd7a6ecea51a2c3c7b1bc829b3
yo <causawey69420@gmail.com>
2026-04-23 12:50:59 +0200

    Prueba listado de elementos de la BD en la página con php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,21 @@
+<?php
+$servername = "dpg-d7jsv5pj2pic73clrgbg-a.frankfurt-postgres.render.com"; // host
+$username = "danmakudle_5w7q_user";
+$password = "jDkoMkOnHXF0ju66VTTaBPF85f9frDyH";
+$dbname = "danmakudle_5w7q";
+
+try {
+    $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Falló: " . $e->getMessage());
+}
+
+$stmt = $conn->query('SELECT * FROM personajes');
+$personajes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
 <html lang='en'>
 <head>
@@ -7,5 +25,12 @@
 </head>
 <body>
     <h1>DANMAKUDLE</h1>
+    <!-- por ahora está en php puro, sin laravel -->
+    <h2>Personajes prueba conexión con la BD</h2>
+    <ul>
+       <?php foreach($personajes as $p): ?>
+            <li>id: <?= %p['id_personaje']?> nombre: <?= $p['nombre'] ?> videojuego: <?= $p['videojuego'] ?></li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
