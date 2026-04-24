ae8d9407c130f4bb0720bd71b3883c919f75d884
suikapon <reikotracker@gmail.com>
2026-04-24 15:40:31 +0200

    ampliación datos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,15 @@ $personajes = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <h2>Personajes prueba conexión con la BD</h2>
     <ul>
        <?php foreach($personajes as $p): ?>
-            <li>id: <?=$p['id_personaje']?> nombre: <?= $p['nombre'] ?> videojuego: <?= $p['videojuego'] ?></li>
+            <li>
+                <br>
+                id: <?=$p['id_personaje']?><br>
+                nombre: <?= $p['nombre'] ?><br>
+                videojuego: <?= $p['videojuego'] ?><br>
+                año lanzamiento: <?= $p['ano_lanzamiento'] ?><br>
+                ubicación: <?= $p['ubicacion'] ?><br>
+                ocupación: <?= $p['ocupacion'] ?><br>
+            </li>
         <?php endforeach; ?>
     </ul>
 </body>
