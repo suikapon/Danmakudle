9ada1cdbe947f375d921d700f35be6b602416654
yo <causawey69420@gmail.com>
2026-04-24 18:24:00 +0200

    quitar laravel

--- public/index.php
+++ public/index.php
@@ -1,20 +1,44 @@
 <?php
+$servername = "dpg-d7jsv5pj2pic73clrgbg-a.frankfurt-postgres.render.com"; // host
+$username = "danmakudle_5w7q_user";
+$password = "jDkoMkOnHXF0ju66VTTaBPF85f9frDyH";
+$dbname = "danmakudle_5w7q";
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+try {
+    $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Falló: " . $e->getMessage());
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+$stmt = $conn->query('SELECT * FROM personajes');
+$personajes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+?>
 
-$app->handleRequest(Request::capture());
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Danmakudle</title>
+</head>
+<body>
+    <h1>DANMAKUDLE</h1>
+    <!-- por ahora está en php puro, sin laravel -->
+    <h2>Personajes prueba conexión con la BD</h2>
+    <ul>
+       <?php foreach($personajes as $p): ?>
+            <li>
+                <br>
+                id: <?=$p['id_personaje']?><br>
+                nombre: <?= $p['nombre'] ?><br>
+                videojuego: <?= $p['videojuego'] ?><br>
+                año lanzamiento: <?= $p['ano_lanzamiento'] ?><br>
+                ubicación: <?= $p['ubicacion'] ?><br>
+                ocupación: <?= $p['ocupacion'] ?><br>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
