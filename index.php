d4d58f176dfd6cade133b6e69a125c8585f64e3d
yo <causawey69420@gmail.com>
2026-04-29 12:56:17 +0200

    href

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<?php
-$servername = "dpg-d7jsv5pj2pic73clrgbg-a.frankfurt-postgres.render.com"; // host
-$username = "danmakudle_5w7q_user";
-$password = "jDkoMkOnHXF0ju66VTTaBPF85f9frDyH";
-$dbname = "danmakudle_5w7q";
-
-try {
-    $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Falló: " . $e->getMessage());
-}
-
-$stmt = $conn->query('SELECT * FROM personajes'); // limite prueba
-$personajes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-
 <!DOCTYPE html>
 <html lang='en'>
 <head>
@@ -25,12 +7,11 @@ $personajes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </head>
 <body>
     <h1>DANMAKUDLE</h1>
-
     <!-- index básico de placeholder -->
     <div class="modos">
         <div class="modo">
             <h2>Listado de personajes</h2>
-            <p><a href="#listapersonajes.php">Lista</a></p>
+            <p><a href="listapersonajes.php">Lista</a></p>
         </div>
         <div class="modo">
             <h2>Modo 2</h2>
