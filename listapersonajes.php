9cddc1f8ec1c52c0d42e8b1fae4e9acc87269ddc
yo <causawey69420@gmail.com>
2026-04-29 12:54:00 +0200

    x2

--- listapersonajes.php
+++ listapersonajes.php
@@ -11,11 +11,21 @@ try {
     die("Falló: " . $e->getMessage());
 }
 
-$stmt = $conn->query('SELECT * FROM personajes ');
+$stmt = $conn->query('SELECT * FROM personajes');
 $personajes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Danmakudle</title>
+</head>
 <body>
+    <h1>DANMAKUDLE</h1>
+    <!-- por ahora está en php puro, sin laravel -->
     <h2>Personajes prueba conexión con la BD</h2>
     <ul>
        <?php foreach($personajes as $p): ?>
