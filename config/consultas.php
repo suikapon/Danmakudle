866901f96d23501ec9be76cd599ad4f149ca9481
suikapon <reikotracker@gmail.com>
2026-05-03 21:16:09 +0200

    prueba random en vez de rand

--- config/consultas.php
+++ config/consultas.php
@@ -15,7 +15,7 @@ function getPersonajeXID($conexion, $id)
 
 function getPersonajeAleatorio($conexion)
 {
-    $stmt = $conexion->prepare("SELECT * FROM personajes ORDER BY RAND() LIMIT 1");
+    $stmt = $conexion->prepare("SELECT * FROM personajes ORDER BY RANDOM() LIMIT 1");
     $stmt->execute();
     return $stmt->get_result()->fetch_assoc();
 }
