3ab4cde8bafd7d664aced918bc9f6c8ac08804d2
suikapon <reikotracker@gmail.com>
2026-05-05 20:35:08 +0200

    fix imagen

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000;">
     <div class="container">
         <a class="navbar-brand" href="/index.php">
-            <img src="/img/danmakudle.png" alt="Danmakudle">
+            <img src="img/danmakudle.png" alt="Danmakudle">
         </a>
     </div>
 </nav>
