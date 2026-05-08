0b5e1219e37c69e0902666327a114f6869565390
suikapon <reikotracker@gmail.com>
2026-05-08 16:34:21 +0200

    header modificado, usuario ficticio, item dropdown para cerrar sesion (placeholder),..

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000;">
     <div class="container-fluid px-4">
         <a class="navbar-brand" href="/index.php">
-            <img src="img/danmakudle.png" alt="Danmakudle">
+            <img src="img/danmakudle.png" alt="Danmakudle" height="20">
         </a>
 
         <!-- los distintos elementos del header -->
@@ -19,7 +19,30 @@
                     <a class="nav-link" href="/rankings.php">Rankings</a>
                 </li>
             </ul>
+
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+                        <i class="bi bi-person-circle"></i>
+                        <span>hola</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <a class="dropdown-item" href="/index.php">hola</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/index.php">hola</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/index.php">hola</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     
     </div>
 </nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"></script>
