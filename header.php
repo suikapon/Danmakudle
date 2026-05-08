1b52ca2dfeb7e62017d53cee3dd31231f1364f76
suikapon <reikotracker@gmail.com>
2026-05-08 15:26:05 +0200

    header cambiado, fuente de letra, estilos añadidos

--- header.php
+++ header.php
@@ -1,7 +1,29 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000;">
-    <div class="container">
+    <div class="container-fluid px-4">
         <a class="navbar-brand" href="/index.php">
             <img src="img/danmakudle.png" alt="Danmakudle">
         </a>
+
+        <!-- los distintos elementos del header -->
+        <div class="collapse navbar-collapse" id="navbarMain">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/index.php">Inicio</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/index.php">Modos</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/index.php">Rankings</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/index.php">Inicio</a>
+                </li>
+            </ul>
+        </div>
+    
     </div>
 </nav>
