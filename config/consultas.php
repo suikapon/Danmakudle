37b54e5f59b617149feeb9d8956714911975d707
yo <causawey69420@gmail.com>
2026-05-17 16:02:20 +0200

    fix consulta nombre de la tabla

--- config/consultas.php
+++ config/consultas.php
@@ -66,7 +66,7 @@ function insertarUsuario($conexion, $username, $email, $password, $rol, $avatar)
 
 function crearEstadisticas($conexion, $id_usuario)
 {
-    $stmt = $conexion->prepare("INSERT INTO estadisticas (id_usuario, partidas_jugadas, partidas_ganadas, racha_actual, racha_max, puntos) VALUES (?, 0, 0, 0, 0, 0)");
+    $stmt = $conexion->prepare("INSERT INTO estadisticas_usuario (id_usuario, partidas_jugadas, partidas_ganadas, racha_actual, racha_max, puntos) VALUES (?, 0, 0, 0, 0, 0)");
     return $stmt->execute([$id_usuario]);
 }
 
