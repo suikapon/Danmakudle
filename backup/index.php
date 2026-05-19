d652e743c58f5424d0cd7bbc59226e34e4214316
suikapon <reikotracker@gmail.com>
2026-05-19 20:45:35 +0200

    index con diarios, creación diarios

--- backup/index.php
+++ backup/index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Danmakudle</title>
-    <?php include 'meta.php'; ?>
+    <?php include 'meta.php';?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -25,65 +25,7 @@
         </div>
 
         <!-- las cajitas/tarjetas con los modos disponibles -->
-
-        <div class="row mt-5">
-            <div class="col">
-                <h2 class="text-center mb-4">Modos Diarios</h2>
-            </div>
-        </div>
-
         <div class="row g-4">
-            <div class="col-md-4 mt-4">
-                <div class="card h-100">
-                    <div class="card-body d-flex flex-column text-center">
-                        <img src="img/reimu.png" class="card-img-top mx-auto mt-6">
-                        <h2>Modo Clásico</h2>
-                        <p class="subtitulo">Adivina el personaje en base a pistas</p>
-                        <a href="modos/diario/modoClasico.php" class="boton stretched-link"></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4">
-                <div class="card h-100">
-                    <div class="card-body d-flex flex-column text-center">
-                        <img src="img/eosd.png" class="card-img-top mx-auto mt-6">
-                        <h2>Modo Videojuegos</h2>
-                        <p class="subtitulo">Adivina el videojuego</p>
-                        <a href="modos/diario/modoPortadas.php" class="boton stretched-link"></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4">
-                <div class="card h-100">
-                    <div class="card-body d-flex flex-column text-center">
-                        <img src="img/silueta.png" class="card-img-top mx-auto mt-6">
-                        <h2>Modo Siluetas</h2>
-                        <p class="subtitulo">Adivina el personaje de la silueta</p>
-                        <a href="modos/diario/modoSiluetas.php" class="boton stretched-link"></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mt-4">
-                <div class="card h-100">
-                    <div class="card-body d-flex flex-column text-center">
-                        <img src="img/temas.png" class="card-img-top mx-auto mt-6">
-                        <h2>Modo Temas</h2>
-                        <p class="subtitulo">Adivina el personaje por su tema</p>
-                        <a href="modos/diario/modoTemas.php" class="boton stretched-link"></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mt-5">
-                <div class="col">
-                    <h2 class="text-center mb-4">Modos Infinitos</h2>
-                </div>
-            </div>
-
-            <div class="row g-4">
             <div class="col-md-4 mt-4">
                 <div class="card h-100">
                     <div class="card-body d-flex flex-column text-center">
@@ -150,7 +92,7 @@
                 </div>
             </div>
 
-            <!-- <div class="col-md-4 mt-4">
+            <div class="col-md-4 mt-4">
                 <div class="card h-100">
                     <div class="card-body d-flex flex-column text-center">
                         <img src="img/017.png" class="card-img-top mx-auto mt-6">
@@ -159,10 +101,8 @@
                         <a href="modoExtra.php" class="boton stretched-link"></a>
                     </div>
                 </div>
-            </div> -->
+            </div>
         </div>
-
-
 
     </main>
 </body>
